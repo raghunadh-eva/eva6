94acb8c8ffac07e86c1de674cdd0a5856e7c1a71
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-04 05:55:42 +0530

    13t push

--- master_repo/main.py
+++ master_repo/main.py
@@ -14,8 +14,8 @@ import albumentations.pytorch as Apy
 import tqdm as tqdm
 import torch.optim.lr_scheduler as StepLR
 
-from models import *
-from utils import *
+from eva6.master_repo.models import *
+from eva6.master_repo.utils import *
 
 test_transforms  = transforms.Compose([
                                        transforms.ToTensor(),
