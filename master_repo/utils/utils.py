3cc67b8289ab13211119005a30279b3a56cd0e1b
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-05 17:44:36 +0530

    jhg

--- master_repo/utils/utils.py
+++ master_repo/utils/utils.py
@@ -189,7 +189,7 @@ class data_albumentations():
     def __init__(self, dataset=datasets.CIFAR10, root="~/data/", train=True, download=True, transform=None):
         super().__init__(dataset=dataset,root=root, train=train, download=download, transform=transform)
     def __getitem__(self, index):
-        image, label = self.data[index], self.targets[index]
+        image, label = self.dataset.data[index], self.dataset.targets[index]
         if self.transform is not None:
             transformed = self.transform(image=image)
             image = transformed["image"]
