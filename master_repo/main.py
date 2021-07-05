54a28bf650ccd2c564486191bf0b1c66e6b210d7
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-05 12:52:11 +0530

    test

--- master_repo/main.py
+++ master_repo/main.py
@@ -14,170 +14,18 @@ import albumentations.pytorch as Apy
 import tqdm as tqdm
 import torch.optim.lr_scheduler as StepLR
 
-import eva6.master_repo.models
-import eva6.master_repo.utils
+#import eva6.master_repo.models
+#import eva6.master_repo.utils
 
-test_transforms  = transforms.Compose([
-                                       transforms.ToTensor(),
-                                       transforms.Normalize((0.4914,0.4822,0.4465), (0.247,0.2435,0.2616))
-                                      ])
+import argparse
 
-train_transforms_a = A.Compose([
-                                       A.Normalize(mean=(0.4914, 0.4822, 0.4465), std=(0.247, 0.2435, 0.2616)),
-                                       A.Sequential([
-                                         A.PadIfNeeded(
-                                             min_height=36,
-                                             min_width=36,
-                                             border_mode=cv2.BORDER_CONSTANT,
-                                             value=(0.4914,0.4822,0.4465)
-                                         ),
-                                         A.RandomCrop(
-                                             height=32,
-                                             width=32
-                                         )
-                                       ], p = 0.5),
-                                       #A.Cutout(num_holes=1,max_h_size=16,max_w_size=16,fill_value=(0.4914,0.4822,0.4465))
-                                       A.CoarseDropout(max_holes=1,max_height=16,max_width=16,min_holes=1,min_height=16,min_width=16,fill_value=(0.4914, 0.4822, 0.4465),mask_fill_value=None),
-                                       Apy.ToTensorV2()
-                                       ])
+def main(argv=None):
+    parser = argparse.ArgumentParser(description="Call the main the function with arguments to perform tranining/validation")
+    parser.add_argument("-d", "--dataset", help="specify the dataset to be used. E.g., MNIST,CIFAR10",required=True)
+    parser.add_argument("-b", "--batch_size", type=int, help="specify the batch_size to be used. default: 64",default=64)
+    parser.add_argument("-e", "--epochs", type=int, help="specify the epochs to be used. default: 1",default=1)
+    parser.add_argument("-drop","--dropout_perc" type=double, help="specify the dropout perc to be used. default: 0.05",default=0.05)
+    parser.add_argument("-opt","--optimizer" , help="Specify the optimizer to use. Specify the short names. default=SGD",default="SGD")
+    parser.add_argument("-sch","--scheduler" , help="Specify the scheduler to use. Specify the short names. default=StepLR",default="StepLR")
 
-def model_summary():
-    cuda = torch.cuda.is_available()
-
-    if cuda:
-      torch.cuda.manual_seed(1)
-
-    device = torch.device("cuda" if cuda else "cpu")
-    model = models.ResNet18()
-    model = model.to(device)
-    summary(model, input_size=(3, 32, 32))
-    optimizer = optim.SGD(model.parameters(), lr=0.02, momentum=0.9)
-
-    scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=20, gamma=0.7)
-
-class data_albumentations(datasets.CIFAR10):
-    def __init__(self, root="~/data/cifar10", train=True, download=True, transform=None):
-        super().__init__(root=root, train=train, download=download, transform=transform)
-
-    def __getitem__(self, index):
-        image, label = self.data[index], self.targets[index]
-
-        if self.transform is not None:
-            transformed = self.transform(image=image)
-            image = transformed["image"]
-
-        return image, label
-
-train = data_albumentations(train=True,  download=True, transform=train_transforms_a)
-test =  datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms)
-
-SEED = 1
-
-#Is GPU ?
-cuda = torch.cuda.is_available()
-
-print('CUDA Available?',cuda)
-
-#what happens when SEED = 2 ?
-torch.manual_seed(SEED)
-
-#set the seed for GPU device as well
-if cuda:
-  torch.cuda.manual_seed(SEED)
-
-device = torch.device("cuda" if cuda else "cpu")
-print(device)
-
-dataloader_args = dict(shuffle=True, batch_size=128, num_workers=2, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
-#Why change batch_size for CPU - since it should not matter
-
-train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
-
-test_loader  = torch.utils.data.DataLoader(test, **dataloader_args)
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-loss_function = nn.CrossEntropyLoss()
-
-def train(model, device, train_loader, optimizer):
-  model.train()
-  pbar = tqdm(train_loader)
-
-  l1_lamda = 0.0001
-
-  correct = 0
-  processed = 0
-  for batch_idx, (data, target) in enumerate(pbar):
-    # get samples
-    data, target = data.to(device), target.to(device)
-
-    # Init
-    optimizer.zero_grad()
-    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes.
-    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
-
-    # Predict
-    y_pred = model(data)
-
-    # Calculate loss
-    #Cross entropy loss
-    #loss = F.nll_loss(y_pred, target)
-    loss = loss_function(y_pred,target)
-    #
-
-    ##Add L1 Loss
-    l1 = 0
-    for p in model.parameters():
-      p_tensor = torch.sum(torch.abs(p))
-      l1 += p_tensor
-
-    loss = loss + l1_lamda * l1
-
-    train_losses.append(loss)
-
-    # Backpropagation
-    loss.backward()
-    optimizer.step()
-
-    # Update pbar-tqdm
-
-    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-    correct += pred.eq(target.view_as(pred)).sum().item()
-    processed += len(data)
-
-    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-    train_acc.append(100*correct/processed)
-
-def test(model, device, test_loader):
-    test_fail_data = []
-    test_fail_target = []
-    test_pred_target = []
-
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            #print(pred,target.view_as(pred))
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            for i,x in enumerate(pred.eq(target.view_as(pred))):
-              if not x:
-                test_fail_data.append(data[i])
-                test_fail_target.append(target[i])
-                test_pred_target.append(pred[i])
-                #print(target[i])
-
-    test_losses.append(test_loss)
-
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-
-    test_acc.append(100. * correct / len(test_loader.dataset))
-
-    return test_losses, test_acc, test_fail_data, test_fail_target, test_pred_target;
+    return args.d,args.b
