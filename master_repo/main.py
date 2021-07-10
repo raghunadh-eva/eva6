0f51f34f4399335a66cb00fa69a212079684f296
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-10 08:46:37 +0530

    asd

--- master_repo/main.py
+++ master_repo/main.py
@@ -184,7 +184,7 @@ else:
     elif args.scheduler == 'ROP':
         scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.7, verbose=True)
     elif args.scheduler == 'OneLR':
-        scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.006,div_factor=1.05,final_div_factor=5, steps_per_epoch=len(train_loader), epochs=args.epochs,verbose=False)
+        scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.006,div_factor=1.15,final_div_factor=5, steps_per_epoch=len(train_loader), epochs=args.epochs,verbose=False)
     else:
         raise Exception("The specified scheduler doesnt exist")
 
