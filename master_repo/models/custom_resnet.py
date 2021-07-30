a2f1c821893b70bc279be0c6e114b3911c1fcf5d
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-30 16:30:07 +0530

    asd

--- master_repo/models/custom_resnet.py
+++ master_repo/models/custom_resnet.py
@@ -22,7 +22,7 @@ class BasicBlock_c(nn.Module):
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
+                               stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         #self.shortcut = nn.Sequential()
