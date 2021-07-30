6f96454caa90c07aca577c3921c2d91ad1b74f07
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-30 14:11:23 +0530

    sd

--- master_repo/main.py
+++ master_repo/main.py
@@ -84,6 +84,24 @@ if args.eva_session == 9:
                                 A.CoarseDropout(max_holes=1,max_height=8,max_width=8,min_holes=1,min_height=8,min_width=8,fill_value=(mean[0], mean[1], mean[2]),mask_fill_value=None),
                                 Apy.ToTensorV2()
                         ])
+    test_transforms_a  = A.Compose([
+                                    A.Normalize(mean=(mean[0], mean[1], mean[2]), std=(std[0], std[1], std[2])),
+                                    A.Sequential([
+                                        A.PadIfNeeded(
+                                            min_height=h+4,
+                                            min_width=w+4,
+                                            border_mode=cv2.BORDER_CONSTANT,
+                                            value=(mean[0],mean[1],mean[2])
+                                            ),
+                                            A.RandomCrop(
+                                            height=h,
+                                            width=w
+                                            )
+                                    ], p = 0.5),
+                                    A.HorizontalFlip(),
+                                    A.CoarseDropout(max_holes=1,max_height=8,max_width=8,min_holes=1,min_height=8,min_width=8,fill_value=(mean[0], mean[1], mean[2]),mask_fill_value=None),
+                                    Apy.ToTensorV2()
+                        ])
 else:
     train_transforms_a = A.Compose([
                                 A.Normalize(mean=(mean[0], mean[1], mean[2]), std=(std[0], std[1], std[2])),
@@ -107,7 +125,7 @@ else:
 
 if args.dataset == 'CIFAR10':
     train_data = data_albumentations_cifar10(root='./data',train=True,download=True, transform=train_transforms_a)
-    test_data =  datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms)
+    test_data =  datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms_a)
 
 elif args.dataset == 'MNIST':
     print("place holder for mnist data")
