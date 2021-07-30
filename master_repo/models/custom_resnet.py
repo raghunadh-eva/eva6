35fedb14f73072f09b37a6cc5a2db086fef5972b
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-30 22:45:48 +0530

    sadf

--- master_repo/models/custom_resnet.py
+++ master_repo/models/custom_resnet.py
@@ -21,7 +21,7 @@ class BasicBlock_c(nn.Module):
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=1,
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
