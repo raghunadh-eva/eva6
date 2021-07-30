a9678001bc1e72f7bd27b813cfa631cac110410a
Raghunadh Puranam <raghunadpuranam@gmail.com>
2021-07-30 17:02:26 +0530

    asdfa

--- master_repo/models/custom_resnet.py
+++ master_repo/models/custom_resnet.py
@@ -21,24 +21,14 @@ class BasicBlock_c(nn.Module):
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=1,
+                               stride=stride, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes),
-                nn.ReLU()
-            )
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out = F.relu(out)
-        out += self.shortcut(out)
         return out
 
 class ResNetCustom(nn.Module):
